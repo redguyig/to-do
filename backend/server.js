31336d607ee6be10421de523cf05c23969256ebf
redguyig <vaibhavkathait5974@gmail.com>
2026-01-24 00:18:40 +0530

    deploying

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -9,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // --- MongoDB Connection ---
-mongoose.connect('mongodb://localhost:27017/tododb')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
